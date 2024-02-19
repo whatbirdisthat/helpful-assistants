1c112120834000f28fa22dd202b2aaa1b337bd35
🐦 <95787+whatbirdisthat@users.noreply.github.com>
2024-02-19 14:35:39 +1100

    a tested whisper client

--- clients/test_WhisperAPI.py
+++ clients/test_WhisperAPI.py
@@ -3,7 +3,18 @@ import unittest
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        expected = "famous quoote from jfk"
+        import os
+        os.environ["OPENAI_ORGANISATION"] = "OPENAI_ORGANISATION"
+        os.environ["OPENAI_API_KEY"] = "OPENAI_API_KEY"
+        os.environ["OPENAI_API_URL"] = "http://127.0.0.1:8080"
+
+        from clients.OpenAiClient import OpenAiClient
+        the_client = OpenAiClient("http://127.0.0.1:8080")
+        with open("docs/examples/jfk.wav", 'rb') as audio_file:
+            actual = the_client.call_openai_whisper_endpoint(audio_file)
+
+        self.assertEqual(actual, expected)  # add assertion here
 
 
 if __name__ == '__main__':
